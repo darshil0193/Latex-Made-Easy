f8aa7266780643c317ded66e44445a60cc83ed4a
Darshil Patel <darshil0193@gmail.com>
2018-02-12 16:08:56 -0500

    chore(build): Setting up the frontend build

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name latexmadeeasyApp
+ * @description
+ * # latexmadeeasyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('latexmadeeasyApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name latexmadeeasyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the latexmadeeasyApp
+ */
+angular.module('latexmadeeasyApp')
+  .controller('MainCtrl', function() {
+
+    console.log('main');
+  });
