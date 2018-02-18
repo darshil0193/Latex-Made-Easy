a332fe34bb90e294ca344c4bbfff7e4346418458
sparasa <sparasa@ncsu.edu>
2018-02-17 22:53:02 -0500

    made multiple backend checks for register and login user module

--- backend/backend.js
+++ backend/backend.js
@@ -21,22 +21,31 @@ app.get('/', (req, res) => {
   res.send('SUCCESS');
 });
 
-app.post('/addToDB', (req, res) => {
-  let username = req.body.username;
+app.post('/registerUser', (req, res) => {
+  let username = req.body.username.toLowerCase();
   let password = req.body.password;
+  let email = req.body.email.toLowerCase();
   MongoClient.connect(uri, (err, client) => {
     const collection = client.db('latex-made-easy-db').collection('users');
     collection.findOne({username: username}, (err, item) => {
       if (item !== null) {
         client.close();
-        res.status(400).send('User already exists');
+        res.status(400).send('Username already taken.');
       } else {
-        collection.insert({username: username, password: password}, (err, item) => {
-          client.close();
-          if (err === null) {
-            res.status(200).send('Added to the database');
-          } else {
-            res.status(400).send('Error adding to database');
+        collection.findOne({email: email}, (err, item) => {
+          if(item !== null){
+            client.close();
+            res.status(400).send('EmailId already used');
+          }
+          else{
+            collection.insert({username: username, password: password, email: email}, (err, item) => {
+              client.close();
+              if(err === null) {
+                res.status(200).send('Added to the database');
+              } else {
+                res.status(400).send('Error adding to database');
+              }
+            });
           }
         });
       }
@@ -44,14 +53,20 @@ app.post('/addToDB', (req, res) => {
   });
 });
 
-app.post('/checkStatus', (req, res) => {
-  let username = req.body.username;
+app.post('/logInUser', (req, res) => {
+  let username = req.body.username.toLowerCase();
+  let password = req.body.password;
   MongoClient.connect(uri, (err, client) => {
     const collection = client.db('latex-made-easy-db').collection('users');
     collection.findOne({username: username}, (err, item) => {
       client.close();
-      if (item !== null) {
-        res.status(200).send('User can login');
+      if(item !== null) {
+        if(item.password === password){
+          res.status(200).send('User can login');
+        }
+        else{
+          res.status(200).send('Password is incorrect');
+        }
       } else {
         res.status(400).send('No such user exists');
       }
