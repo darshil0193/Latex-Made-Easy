e567507a2a8d4ab1ddc3ce10ada7887f3915f730
harshavardhan0605 <32597583+harshavardhan0605@users.noreply.github.com>
2018-03-12 19:00:41 -0400

    Revert "tests(unit): Added unit tests"

--- test/spec/controllers/add-list-ctrl.spec.js
+++ test/spec/controllers/add-list-ctrl.spec.js
@@ -1,21 +1,17 @@
 'use strict';
 
-describe('Controller: AddListCtrl', () => {
+describe('Controller: AddListCtrl', function() {
 
   // load the controller's module
   beforeEach(module('latexmadeeasyApp'));
 
-  let AddListCtrl, $q, $mdDialog, $timeout, $httpBackend;
+  let AddListCtrl;
   let scope = {};
   let element = angular.element('<div></div>');
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(($controller, _$rootScope_, _$mdDialog_, _$q_, _$timeout_, _$httpBackend_) => {
-    scope = _$rootScope_.$new();
-    $q = _$q_;
-    $mdDialog = _$mdDialog_;
-    $httpBackend = _$httpBackend_;
-    $timeout = _$timeout_;
+  beforeEach(inject(function($controller, $rootScope) {
+    scope = $rootScope.$new();
     AddListCtrl = $controller('AddListCtrl', {
       $scope: scope,
       $element: element
@@ -24,79 +20,7 @@ describe('Controller: AddListCtrl', () => {
     });
   }));
 
-  beforeEach(() => {
-    AddListCtrl.list = {
-      moduleId: 1,
-      id: 1,
-      type: 'list',
-      items: [{
-        id: 1,
-        text: ''
-      }],
-      deleted: false,
-      ordered: false
-    };
-
-    AddListCtrl.items = AddListCtrl.list.items.length;
-  });
-
-  it('should increment items when an item is added', () => {
-    expect(AddListCtrl.items).toEqual(1);
-    spyOn(AddListCtrl.list.items, 'push');
-    AddListCtrl.addItem();
-    expect(AddListCtrl.items).toEqual(2);
-    expect(AddListCtrl.list.items.push).toHaveBeenCalledWith({
-      id: AddListCtrl.items,
-      text: ''
-    });
-  });
-
-  it('should pop up a dialog when user tries to remove an item', () => {
-    spyOn($mdDialog, 'show').and.returnValue($q.when());
-    AddListCtrl.removeLastItem();
-    expect($mdDialog.show).toHaveBeenCalled();
-  });
-
-  it('should decrement items when removeLastItem is called', () => {
-    expect(AddListCtrl.items).toEqual(1);
-    spyOn($mdDialog, 'show').and.returnValue($q.when());
-    spyOn(AddListCtrl.list.items, 'pop');
-    $httpBackend.when('GET', 'views/main.html').respond('');
-    AddListCtrl.removeLastItem();
-    $timeout.flush();
-    expect(AddListCtrl.items).toEqual(0);
-    expect(AddListCtrl.list.items.pop).toHaveBeenCalled();
-  });
-
-  it('should pop up a dialog when user tries to remove the list', () => {
-    spyOn($mdDialog, 'show').and.returnValue($q.when());
-    AddListCtrl.removeList();
-    expect($mdDialog.show).toHaveBeenCalled();
-  });
-
-  it('should mark the list as deleted', () => {
-    spyOn($mdDialog, 'show').and.returnValue($q.when());
-    $httpBackend.when('GET', 'views/main.html').respond('');
-    AddListCtrl.removeList();
-    $timeout.flush();
-    expect(AddListCtrl.list.deleted).toBeTruthy();
-  });
-
-  it('should remove the element when removeList is called', () => {
-    spyOn($mdDialog, 'show').and.returnValue($q.when());
-    spyOn(AddListCtrl.$element, 'remove');
-    $httpBackend.when('GET', 'views/main.html').respond('');
-    AddListCtrl.removeList();
-    $timeout.flush();
-    expect(AddListCtrl.$element.remove).toHaveBeenCalled();
-  });
-
-  it('should destroy the scope of the element when removeList is called', () => {
-    spyOn($mdDialog, 'show').and.returnValue($q.when());
-    spyOn(AddListCtrl.$scope, '$destroy');
-    $httpBackend.when('GET', 'views/main.html').respond('');
-    AddListCtrl.removeList();
-    $timeout.flush();
-    expect(AddListCtrl.$scope.$destroy).toHaveBeenCalled();
+  it('should attach a list of awesomeThings to the scope', function() {
+    expect(true).toBe(true);
   });
 });
