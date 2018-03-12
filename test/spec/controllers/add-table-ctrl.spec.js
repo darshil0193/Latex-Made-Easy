8e0e8991bbbdbf2d8525ffa14e9ca05b71d4e095
Harshavardhan Reddy <muppidi.harshavardhan@gmail.com>
2018-03-12 19:27:28 -0400

    tests(unit): Added unit tests

--- test/spec/controllers/add-table-ctrl.spec.js
+++ test/spec/controllers/add-table-ctrl.spec.js
@@ -4,13 +4,17 @@ describe('Controller: AddtableCtrl', function() {
   // load the controller's module
   beforeEach(module('latexmadeeasyApp'));
 
-  let AddTableCtrl;
+  let AddTableCtrl, $mdDialog, $q, $timeout, $httpBackend;
   let scope = {};
   let element = angular.element('<div></div>');
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function($controller, $rootScope) {
-    scope = $rootScope.$new();
+  beforeEach(inject(function($controller, _$rootScope_, _$mdDialog_, _$q_, _$timeout_, _$httpBackend_) {
+    scope = _$rootScope_.$new();
+    $mdDialog = _$mdDialog_;
+    $q = _$q_;
+    $timeout = _$timeout_;
+    $httpBackend = _$httpBackend_;
     AddTableCtrl = $controller('AddTableCtrl', {
       $scope: scope,
       $element: element
@@ -19,7 +23,123 @@ describe('Controller: AddtableCtrl', function() {
     });
   }));
 
-  it('should pass', function() {
-    expect(true).toBe(true);
+  beforeEach(() => {
+    AddTableCtrl.table = {
+      moduleId: this.moduleNumber,
+      id: this.tableNumber,
+      type: 'table',
+      caption: '',
+      deleted: false,
+      grid: {
+        enableColumnMenus: false,
+        selectionRowHeaderWidth: 35,
+        rowHeight: 35,
+        enableSorting: false,
+        columnDefs: [{name: 'Column 1'}],
+        data: [{'Column 1': 'Something in row 1 column 1'}]
+      }
+    };
+    AddTableCtrl.isAddCaption = false;
+    AddTableCtrl.captionAdded = false;
+  });
+
+  it('should pop up a dialog to ask the column name when addColumn is called', function() {
+    spyOn($mdDialog, 'show').and.returnValue($q.when());
+    AddTableCtrl.addColumn();
+    expect($mdDialog.show).toHaveBeenCalled();
+  });
+
+  it('should add a column to ColumnDefs when addColumn is called', () => {
+    expect(AddTableCtrl.table.grid.columnDefs).toEqual([{
+      name: 'Column 1'
+    }]);
+    spyOn($mdDialog, 'show').and.callFake(() => {
+      let deferred = $q.defer();
+      deferred.resolve('Test Column');
+      return deferred.promise;
+    });
+    $httpBackend.when('GET', 'views/main.html').respond('');
+    AddTableCtrl.addColumn();
+    $timeout.flush();
+    expect(AddTableCtrl.table.grid.columnDefs).toEqual([{
+      name: 'Column 1'
+    }, {
+      name: 'Test Column'
+    }]);
+  });
+
+  it('should add row data when addRow is called', () => {
+    AddTableCtrl.table.grid.columnDefs.push({
+      name: 'Test Column'
+    });
+    AddTableCtrl.addRow();
+    expect(AddTableCtrl.table.grid.data).toEqual([{
+      'Column 1': 'Something in row 1 column 1'
+    }, {
+      'Column 1': 'Double Click to Edit',
+      'Test Column': 'Double Click to Edit'
+    }]);
+  });
+
+  it('should mark the flags true when caption is added', () => {
+    expect(AddTableCtrl.isAddCaption).toBeFalsy();
+    expect(AddTableCtrl.captionAdded).toBeFalsy();
+    AddTableCtrl.addCaption();
+    expect(AddTableCtrl.isAddCaption).toBeTruthy();
+    expect(AddTableCtrl.captionAdded).toBeTruthy();
+  });
+
+  it('should pop up a dialog when removeCaption is called', () => {
+    spyOn($mdDialog, 'show').and.returnValue($q.when());
+    AddTableCtrl.removeCaption();
+    expect($mdDialog.show).toHaveBeenCalled();
+  });
+
+  it('should mark the flags false when removeCaption is called', () => {
+    AddTableCtrl.isAddCaption = true;
+    AddTableCtrl.captionAdded = true;
+    AddTableCtrl.table.caption = 'Test Caption';
+    expect(AddTableCtrl.isAddCaption).toBeTruthy();
+    expect(AddTableCtrl.captionAdded).toBeTruthy();
+    spyOn($mdDialog, 'show').and.returnValue($q.when());
+    $httpBackend.when('GET', 'views/main.html').respond('');
+    AddTableCtrl.removeCaption();
+    $timeout.flush();
+    expect(AddTableCtrl.isAddCaption).toBeFalsy();
+    expect(AddTableCtrl.captionAdded).toBeFalsy();
+    expect(AddTableCtrl.table.caption).toEqual('');
+  });
+
+  it('should pop up a dialog when removeTable is called', () => {
+    spyOn($mdDialog, 'show').and.returnValue($q.when());
+    AddTableCtrl.removeTable();
+    expect($mdDialog.show).toHaveBeenCalled();
+  });
+
+  it('should mark the table deleted when removeTable is called', () => {
+    expect(AddTableCtrl.table.deleted).toBeFalsy();
+    spyOn($mdDialog, 'show').and.returnValue($q.when());
+    $httpBackend.when('GET', 'views/main.html').respond('');
+    AddTableCtrl.removeTable();
+    $timeout.flush();
+    expect(AddTableCtrl.table.deleted).toBeTruthy();
+  });
+
+  it('should remove the element when removeTable is called', () => {
+    spyOn($mdDialog, 'show').and.returnValue($q.when());
+    spyOn(AddTableCtrl.$element, 'remove');
+    $httpBackend.when('GET', 'views/main.html').respond('');
+    AddTableCtrl.removeTable();
+    $timeout.flush();
+    expect(AddTableCtrl.$element.remove).toHaveBeenCalled();
+  });
+
+  it('should destroy the scope of the element when removeTable is called', () => {
+    spyOn($mdDialog, 'show').and.returnValue($q.when());
+    spyOn(AddTableCtrl.$scope, '$destroy');
+    $httpBackend.when('GET', 'views/main.html').respond('');
+    AddTableCtrl.removeTable();
+    $timeout.flush();
+    expect(AddTableCtrl.$scope.$destroy).toHaveBeenCalled();
   });
 });
--- test/spec/controllers/login-ctrl.spec.js
+++ test/spec/controllers/login-ctrl.spec.js
@@ -4,11 +4,16 @@ describe('Controller: LoginCtrl', function() {
   // load the controller's module
   beforeEach(module('latexmadeeasyApp'));
 
-  let LoginCtrl, scope;
+  let LoginCtrl, $rootScope, scope, constants, $httpBackend, $mdDialog, $q;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function($controller, $rootScope) {
+  beforeEach(inject(function($controller, _$rootScope_, _$httpBackend_, _$mdDialog_, _$q_, _constants_) {
+    $rootScope = _$rootScope_;
     scope = $rootScope.$new();
+    constants = _constants_;
+    $httpBackend = _$httpBackend_;
+    $mdDialog = _$mdDialog_;
+    $q = _$q_;
     LoginCtrl = $controller('LoginCtrl', {
       $scope: scope
 
@@ -16,7 +21,218 @@ describe('Controller: LoginCtrl', function() {
     });
   }));
 
-  it('should pass', function() {
-    expect(true).toBe(true);
+  it('should show login form when showLoginForm is called', function() {
+    LoginCtrl.isRegister = true;
+    LoginCtrl.failureMessage = 'some message';
+    LoginCtrl.showLoginForm();
+    expect(LoginCtrl.failureMessage).toEqual('');
+    expect(LoginCtrl.isRegister).toBeFalsy();
+  });
+
+  it('should show registration form when showRegistrationForm is called', function() {
+    LoginCtrl.isRegister = false;
+    LoginCtrl.failureMessage = 'some message';
+    LoginCtrl.showRegistrationForm();
+    expect(LoginCtrl.failureMessage).toEqual('');
+    expect(LoginCtrl.isRegister).toBeTruthy();
+  });
+
+  it('should show forgot password form when showForgotPasswordForm is called', function() {
+    LoginCtrl.isRegister = true;
+    LoginCtrl.isForgotPassword = false;
+    LoginCtrl.failureMessage = 'some message';
+    LoginCtrl.showForgotPasswordForm();
+    expect(LoginCtrl.failureMessage).toEqual('');
+    expect(LoginCtrl.isRegister).toBeFalsy();
+    expect(LoginCtrl.isForgotPassword).toBeTruthy();
+  });
+
+  describe('Send Email', () => {
+
+    beforeEach(() => {
+      LoginCtrl.credentials = {
+        username: 'test',
+        password: 'test',
+        email: 'test@email.com'
+      };
+    });
+
+    it('should call the backend email service', () => {
+      LoginCtrl.isForgotPassword = true;
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/sendEmail').respond({data: {email: LoginCtrl.credentials.email}});
+      LoginCtrl.sendEmail(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.isForgotPassword).toBeFalsy();
+    });
+
+    it('should show the backend errors if email sending fails', () => {
+      LoginCtrl.isForgotPassword = true;
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/sendEmail').respond(400, {error: 'EMAIL_NOT_SENT', email: LoginCtrl.credentials.email});
+      LoginCtrl.sendEmail(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.isForgotPassword).toBeFalsy();
+      expect(LoginCtrl.failureTitle).toEqual('Sorry!');
+      expect(LoginCtrl.failureMessage).toEqual('There seems to be some trouble with sending the email to ' + LoginCtrl.credentials.email + '. We are working on it.');
+    });
+
+    it('should show the backend errors if username is incorrect', () => {
+      LoginCtrl.isForgotPassword = true;
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/sendEmail').respond(400, {error: 'USER_INCORRECT'});
+      LoginCtrl.sendEmail(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.isForgotPassword).toBeFalsy();
+      expect(LoginCtrl.failureTitle).toEqual('Username Error');
+      expect(LoginCtrl.failureMessage).toEqual('Incorrect Username Entered. Such a user does not exist');
+    });
+  });
+
+  describe('Login', () => {
+
+    beforeEach(() => {
+      LoginCtrl.credentials = {
+        username: 'test',
+        password: 'test',
+        email: 'test@email.com'
+      };
+    });
+
+    it('should call the backend login service', () => {
+      let frontBlockDataResponse = {
+        title: {
+          title: '',
+          author: '',
+          college: '',
+          degree: '',
+          date: new Date()
+        },
+        dedication: {
+          dedication: ''
+        },
+        acknowledge: {
+          acknowledge:''
+        },
+        abstract: {
+          abstract: ''
+        },
+        chapters: {
+          chapters: []
+        }
+      };
+      LoginCtrl.successfulLogin = false;
+
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/loginUser').respond({latexJson: frontBlockDataResponse});
+      LoginCtrl.login(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect(LoginCtrl.successfulLogin).toBeTruthy();
+      expect($rootScope.currentUser).toEqual('test');
+      expect(LoginCtrl.frontBlockData).toEqual(frontBlockDataResponse);
+    });
+
+    it('should show the backend error when password is incorrect', () => {
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/loginUser').respond(400, {error: 'PASS_INCORRECT'});
+      LoginCtrl.login(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.failureTitle).toEqual('Password Error');
+      expect(LoginCtrl.failureMessage).toEqual('Incorrect Password Entered');
+    });
+
+    it('should show the backend error when username is incorrect', () => {
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/loginUser').respond(400, {error: 'USER_INCORRECT'});
+      LoginCtrl.login(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.failureTitle).toEqual('Username Error');
+      expect(LoginCtrl.failureMessage).toEqual('Incorrect Username Entered. Such a user does not exist');
+    });
+  });
+
+  describe('Register', () => {
+
+    beforeEach(() => {
+      LoginCtrl.credentials = {
+        username: 'test',
+        password: 'test',
+        email: 'test@email.com'
+      };
+    });
+
+    it('should call backend service for registering', () => {
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/registerUser').respond();
+      LoginCtrl.register(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect(LoginCtrl.successfulLogin).toBeTruthy();
+      expect($rootScope.currentUser).toEqual('test');
+    });
+
+    it('should throw error if username is already taken', () => {
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/registerUser').respond(409, {error: 'UNAME_TAKEN'});
+      LoginCtrl.register(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.failureTitle).toEqual('Username Error');
+      expect(LoginCtrl.failureMessage).toEqual('Username already taken. Please try something else');
+    });
+
+    it('should throw error if username is already taken', () => {
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/registerUser').respond(409, {error: 'EMAIL_USED'});
+      LoginCtrl.register(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.failureTitle).toEqual('Email Error');
+      expect(LoginCtrl.failureMessage).toEqual('Email ID already used. Please Login or try with a different email id');
+    });
+
+    it('should throw error if password length is less than 8 characters', () => {
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/registerUser').respond(400, {error: 'PASS_LENGTH'});
+      LoginCtrl.register(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.failureTitle).toEqual('Password Error');
+      expect(LoginCtrl.failureMessage).toEqual('Password should be at least 8 characters in length');
+    });
+
+    it('should throw error if password contains spaces', () => {
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/registerUser').respond(400, {error: 'PASS_SPACE'});
+      LoginCtrl.register(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.failureTitle).toEqual('Password Error');
+      expect(LoginCtrl.failureMessage).toEqual('Password can not contain spaces');
+    });
+
+    it('should throw error if password contains some special characters', () => {
+      spyOn($mdDialog, 'show').and.returnValue($q.when());
+      $httpBackend.when('GET', 'views/main.html').respond('');
+      $httpBackend.when('POST', constants.backendUrl + '/registerUser').respond(400, {error: 'PASS_CHARS'});
+      LoginCtrl.register(LoginCtrl.credentials);
+      $httpBackend.flush();
+      expect($mdDialog.show).toHaveBeenCalled();
+      expect(LoginCtrl.failureTitle).toEqual('Password Error');
+      expect(LoginCtrl.failureMessage).toEqual('Password cannot contain any of //, /* or */ patterns');
+    });
   });
 });
